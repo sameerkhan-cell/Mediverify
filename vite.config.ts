e35bd7f30aed226ca6db7ba8e6584c4b410054f2
Sameer Khan <sameerkhan031181@gmail.com>
2026-06-29 16:34:06 +0500

    fix: map nitro output directories to vercel output structure

--- vite.config.ts
+++ vite.config.ts
@@ -28,5 +28,10 @@ export default defineConfig({
   },
   nitro: {
     preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions/index.func",
+      publicDir: ".vercel/output/static",
+    }
   },
 });
